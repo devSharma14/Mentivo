e12d55849fac079551708fd3e616895b04de1e3f
Dev Sharma <devs140124@gmail.com>
2026-03-05 18:05:17 +0530

    feat: add Find mentor page

---
+++ app/mentorProfile/page.tsx
@@ -0,0 +1,13 @@
+import Calendar from '../../public/assets/calendar.svg';
+import User from '../../public/assets/user.svg'
+import Twitter from '../../public/assets/twitter.svg'
+import LinkedIn from '../../public/assets/linkedin.svg'
+import Experience from '../../public/assets/experience.svg'
+import Clock from '../../public/assets/clock.svg'
+
+
+export default function MentorProfile(){
+    // return(
+       
+    // );
+}
